b387c81b303c5c118e911d604ff3fe7d2375b1e1
dwidoo <sultandavid@free.fr>
2023-05-09 23:40:56 +0000

    fixed os link and indentation end of listing

--- pages/01_🌊OpenSea Listings.py
+++ pages/01_🌊OpenSea Listings.py
@@ -138,22 +138,23 @@ try:
     listings_df["🛒 Discount %"] = (listings_df["🤑 veCHR Value in USD"] -
                                    listings_df["💰 Sale Price in USD"]) / listings_df["🤑 veCHR Value in USD"] * 100
     listings_df["🔗 OS Link"] = listings_df["🔢 Token ID"].apply(
-        lambda x: '<a href="https://opensea.io/assets/bsc/0x9A01857f33aa382b1d5bb96C3180347862432B0d/' + str(x) + '">OS Link</a>')
+        lambda x: '<a href="https://opensea.io/assets/arbitrum/0x9A01857f33aa382b1d5bb96C3180347862432B0d/' + str(x) + '">OS Link</a>')
     listings_df.drop(columns=["✔️ Vote Reset"], inplace=True)
     listings_df.sort_values(by="🛒 Discount %", ascending=False, inplace=True)
 except Exception as e:
     print(e)
 
-    # creating a single-element container
-    placeholder = st.empty()
+# creating a single-element container
+placeholder = st.empty()
 
-    # Empty Placeholder Filled
-    with placeholder.container():
-        st.write(listings_df.to_html(escape=False, index=False, float_format="{:10.2f}".format), unsafe_allow_html=True)
-        # Note
-        st.markdown("#")
-        st.markdown("#")
-        st.caption(
+# Empty Placeholder Filled
+with placeholder.container():
+    st.write(listings_df.to_html(escape=False, index=False, float_format="{:10.2f}".format), unsafe_allow_html=True)
+
+# Note
+st.markdown("#")
+st.markdown("#")
+st.caption(
     """
 NFA, DYOR -- This web app is in beta, I am not responsible for any information on this page.
 
